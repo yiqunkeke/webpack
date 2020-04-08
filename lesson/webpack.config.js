d3640f0548699e7f2c5affe9898d4feac066e9bc
unknown <1431806069@qq.com>
2020-04-08 15:23:07 +0800

    webpack打包输出内容

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -3,8 +3,16 @@
 const path = require('path');
 
 module.exports = {
+    // mode 的默认值就是 production
+    // 如果不配置 mode ，在打包时会发生警告。
+    // 如果设置mode 为 production,则打包出的文件就会被压缩成一行。
+    // 设置mode 为 development，则打包出来的文件就不会被压缩。
+    mode: 'development',
     // 项目要从哪个文件开始打包
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        main: './src/index.js'
+    },
     output: {
         filename: 'bundle.js', // 打包后生成的文件名
         // path: 打包出的文件要放置到哪个文件夹下，path 必须是一个绝对路径
