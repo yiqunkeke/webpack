e1264c1cb966b813ce3323a76f780e1bbe119f03
coco <1431806069@qq.com>
2020-04-12 21:29:16 +0800

    entry-output

--- lesson/webpack.config.js
+++ lesson/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
     // 项目要从哪个文件开始打包
     // entry: './src/index.js',
     entry: {
-        main: './src/index.js'
+        main: './src/index.js',
+        sub: './src/index.js'
     },
     // 非 js 模块打包配置
     module: {
@@ -35,10 +36,10 @@ module.exports = {
                 ]
             },
             // css文件
-            // {
-            //     test: /\.css$/,
-            //     use: ['style-loader', 'css-loader']
-            // },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
             // scss 文件
             {
                 test: /\.scss$/,
@@ -72,7 +73,7 @@ module.exports = {
         new CleanWebpackPlugin()
     ],
     output: {
-        filename: 'bundle.js', // 打包后生成的文件名
+        filename: '[name].js', // 打包后生成的文件名
         // path: 打包出的文件要放置到哪个文件夹下，path 必须是一个绝对路径
         // 所以需要引入 node.js 中 path 模块并调用模块的 resolve()方法来得到一个绝对路径
         path: path.resolve(__dirname, 'dist')
